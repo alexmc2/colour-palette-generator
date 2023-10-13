1f14415fd0a77a8cd577ad1fd95ceb91587c8f6b
Alex <alex_mcgarry@hotmail.co.uk>
2023-10-13 18:41:25 +0100

    return colours

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request
+import os
+import pprint
+import re
+import openai
+from dotenv import load_dotenv
+from flask import jsonify
+
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+app = Flask(__name__, template_folder="templates")
+
+
+def generate_palette(msg):
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a color palette generating assistant that helps designers find the perfect color palette for their design. Please provide hexadecimal color codes for each color suggestion.",
+            },
+            {"role": "user", "content": msg},
+        ],
+    )
+
+    message_content = completion.choices[0].message["content"]
+    palette = re.findall(r"#[0-9A-Fa-f]{6}", message_content)
+    return palette
+
+
+@app.route("/palette", methods=["POST"])
+def palette_prompt():
+    app.logger.info("Post request received!")
+    query = request.form.get("query")
+    colors = generate_palette(query)
+    return jsonify({"colors": colors})
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
