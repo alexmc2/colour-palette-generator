59e1827a8eb6c3f56cda82c902d02497eaff78cd
Alex <alex_mcgarry@hotmail.co.uk>
2023-10-15 22:22:25 +0100

    random and complementary colour function

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import openai
 from dotenv import load_dotenv
 from flask import jsonify
 import time
+import random
 
 
 load_dotenv()
@@ -60,3 +61,60 @@ def palette_prompt():
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+# def random_color():
+
+#     # Generate a random hex color code.
+
+#     return "#{:06x}".format(random.randint(0, 0xFFFFFF))
+
+
+# def is_valid_hex_color(color):
+
+#     # Check if the provided string is a valid hex color code.
+
+#     return bool(re.match(r"^#(?:[0-9a-fA-F]{3}){1,2}$", color))
+
+
+# def complementary_color(hex_color):
+
+#     # Compute the complementary color of the given hex color code.
+
+#     # Ensure the color is valid
+#     if not is_valid_hex_color(hex_color):
+#         raise ValueError("Invalid hex color code.")
+
+#     # Convert the hex code to RGB values
+#     r = int(hex_color[1:3], 16)
+#     g = int(hex_color[3:5], 16)
+#     b = int(hex_color[5:7], 16)
+
+#     comp_r = 255 - r
+#     comp_g = 255 - g
+#     comp_b = 255 - b
+
+#     return "#{:02X}{:02X}{:02X}".format(comp_r, comp_g, comp_b)
+
+
+# @app.route("/random_color", methods=["GET"])
+# def random_color_route():
+
+#     # Generate and return a random color code.
+
+#     color = random_color()
+#     return jsonify({"color": color})
+
+
+# @app.route("/complementary", methods=["POST"])
+# def complementary_color_route():
+
+#     # compute and return complementary color.
+
+#     data = request.get_json()
+#     hex_color = data.get("color")
+#     try:
+#         complementary = complementary_color(hex_color)
+#         return jsonify({"complementary": complementary})
+#     except ValueError:
+#         return jsonify({"error": "Invalid color provided."}), 400
